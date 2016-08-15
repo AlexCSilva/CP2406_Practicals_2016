92efa5e31f5ace9037fe9054a26de3a4728e6187
jc282222 <alexander.silva@my.jcu.edu.au>
2016-08-15 14:16:24 +1000

    Changed 'static int' to 'static void' for each method as nothing needs to be returned

--- Chapter3/ProgrammingExercises/NumbersDemo.java
+++ Chapter3/ProgrammingExercises/NumbersDemo.java
@@ -12,17 +12,17 @@ public class NumbersDemo {
         displayTwiceTheNumber(num2);
         displayNumberSquared(num2);
     }
-    public static int displayTwiceTheNumber(int n)
+    public static void displayTwiceTheNumber(int n)
     {
         final int FACTOR = 2;
         System.out.println(n + " times " + FACTOR + " is " + (n +FACTOR));
     }
-    public static int displayNumberPlusFive(int n)
+    public static void displayNumberPlusFive(int n)
     {
         final int FACTOR = 5;
         System.out.println(n + " plus " + FACTOR + " is " + (n * FACTOR));
     }
-    public static int displayNumberSquared(int n)
+    public static void displayNumberSquared(int n)
     {
         System.out.println(n + " squared is " + (n * n));
     }
