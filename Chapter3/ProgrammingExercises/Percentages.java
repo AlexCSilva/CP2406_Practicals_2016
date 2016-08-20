61537e07c04753796767b701fb0b0f1ea08e1ba9
AlexCSilva <alexander.silva@my.jcu.edu.au>
2016-08-20 16:24:36 +1000

    start writting Percentages class

---
+++ Chapter3/ProgrammingExercises/Percentages.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Smoo on 20-Aug-16.
+ */
+public class Percentages {
+    public static void main(String args[])
+    {
+        int num1 = 10, num2 = 20;
+
+    }
+    public static void computePercentage()
+    {
+        //stuffgoes here
+    }
+}
