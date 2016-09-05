34e1fc691c94d70b4f3cec6097052eb1de9b361d
jc282222 <alexander.silva@my.jcu.edu.au>
2016-09-05 14:28:00 +1000

    Written Horse and RaceHorse classes to test with DemoHorses

---
+++ Chapter10/ProgrammingExercises/RaceHorse.java
@@ -0,0 +1,15 @@
+/**
+ * Created by jc282222 on 5/09/16.
+ */
+public class RaceHorse extends Horse
+{
+    private int races;
+    public int getRaces()
+    {
+        return races;
+    }
+    public void setRaces(int r)
+    {
+        races = r;
+    }
+}
